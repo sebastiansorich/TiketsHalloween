ce3500ea5c36d250daf54295b74f01faf5a8fa4c
Sebastian <sebastiansorich@gmail.com>
2025-10-22 20:18:07 -0400

    prueba 1.1

--- src/controllers/ticket_controller.py
+++ src/controllers/ticket_controller.py
@@ -264,13 +264,13 @@ def generate_invitation_with_qr(token):
         try:
             # Buscar fuentes del sistema que se parezcan al estilo del arte
             font_large = ImageFont.truetype("arial.ttf", 320)  # Aumentado de 80 a 320
-            font_medium = ImageFont.truetype("arial.ttf", 150)  # Aumentado de 50 a 150
-            font_small = ImageFont.truetype("arial.ttf", 90)   # Aumentado de 30 a 90
+            font_medium = ImageFont.truetype("arial.ttf", 320)  # Aumentado de 50 a 150
+            font_small = ImageFont.truetype("arial.ttf", 320)   # Aumentado de 30 a 90
         except:
             try:
-                font_large = ImageFont.truetype("C:/Windows/Fonts/arial.ttf", 120)
-                font_medium = ImageFont.truetype("C:/Windows/Fonts/arial.ttf", 80)
-                font_small = ImageFont.truetype("C:/Windows/Fonts/arial.ttf", 50)
+                font_large = ImageFont.truetype("C:/Windows/Fonts/arial.ttf", 320)
+                font_medium = ImageFont.truetype("C:/Windows/Fonts/arial.ttf", 320)
+                font_small = ImageFont.truetype("C:/Windows/Fonts/arial.ttf", 320)
             except:
                 # Fuente por defecto si no se encuentra arial
                 font_large = ImageFont.load_default()
