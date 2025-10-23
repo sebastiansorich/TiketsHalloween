bd1a7edc0d8175ded8a8074d68b46b7fd0809e86
Sebastian <sebastiansorich@gmail.com>
2025-10-22 22:15:10 -0400

    cambie otra vez jaja

--- src/controllers/ticket_controller.py
+++ src/controllers/ticket_controller.py
@@ -248,11 +248,11 @@ def generate_invitation_with_qr(token):
         qr_img = qr_img.resize(qr_size)
 
         # 🔄 Rotar en dirección opuesta (hacia la izquierda) sin fondo negro
-        qr_img = qr_img.rotate(4, expand=True, fillcolor=(255, 255, 255, 0))
+        qr_img = qr_img.rotate(7, expand=True, fillcolor=(255, 255, 255, 0))
 
         # 📍 Posicionar más a la derecha y hacia abajo
-        qr_x = (background.width - qr_img.width) // 2 + 32
-        qr_y = int(background.height * 0.48)
+        qr_x = (background.width - qr_img.width) // 2 + 34
+        qr_y = int(background.height * 0.46)
 
         # 🧩 Combinar sin opacidad adicional
         background.alpha_composite(qr_img, (qr_x, qr_y))
